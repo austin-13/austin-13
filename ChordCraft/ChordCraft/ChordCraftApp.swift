f465766ac2e6339e83b032e4976e3e5b412b6da0
austin-13 <122950600+austin-13@users.noreply.github.com>
2025-01-20 21:39:19 -0700

    Add files via upload

---
+++ ChordCraft/ChordCraft/ChordCraftApp.swift
@@ -0,0 +1,38 @@
+//
+//  ChordCraftApp.swift
+//  ChordCraft
+//
+//
+
+import SwiftData
+import SwiftUI
+
+@main
+struct ChordCraftApp: App {
+    // inital model setup for database
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            song.self, Project.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false) // set  isStoredInMemoryOnly to false  to keep stuff forever,
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+           // AddsongForm()
+           ContentView() // main
+         //  showSongsTest()
+        //AddProjectForm()
+        }
+        .windowStyle(HiddenTitleBarWindowStyle()) // hides the title bar
+        .modelContainer(sharedModelContainer)
+        
+    }
+   
+}
